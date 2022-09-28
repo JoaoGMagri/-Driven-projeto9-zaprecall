f682a1361754203b1cb9339563c34a8fc6a63f79
JoaoGMagri <jgmagri@hotmail.com>
2022-09-28 20:30:22 -0300

    Versão 1.5

--- src/components/objetos/PeguntasJS.js
+++ src/components/objetos/PeguntasJS.js
@@ -1,23 +1,71 @@
 const perguntasJS = [
     [
-        {pergunta:"O que é JSX?", resposta:"Uma extensão de linguagem do JavaScript"},
-        {pergunta:"O React é __?", resposta:"uma biblioteca JavaScript para construção de interfaces"},
-        {pergunta:"Componentes devem iniciar com __", resposta:"letra maiúscula"},
-        {pergunta:"Podemos colocar __ dentro do JSX", resposta:"expressões"},
-        {pergunta:"O ReactDOM nos ajuda __", resposta:"interagindo com a DOM para colocar componentes React na mesma"},
-        {pergunta:"Usamos o npm para __", resposta:"gerenciar os pacotes necessários e suas dependências"},
-        {pergunta:"Usamos props para __", resposta:"passar diferentes informações para componentes"},
-        {pergunta:"Usamos estado (state) para __", resposta:"dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"}
+        {
+            pergunta: "O que é JSX?",
+            resposta: "Uma extensão de linguagem do JavaScript"
+        },
+        {
+            pergunta: "O React é __?",
+            resposta: "uma biblioteca JavaScript para construção de interfaces"
+        },
+        {
+            pergunta: "Componentes devem iniciar com __",
+            resposta: "letra maiúscula"
+        },
+        {
+            pergunta: "Podemos colocar __ dentro do JSX",
+            resposta: "expressões"
+        },
+        {
+            pergunta: "O ReactDOM nos ajuda __",
+            resposta: "interagindo com a DOM para colocar componentes React na mesma"
+        },
+        {
+            pergunta: "Usamos o npm para __",
+            resposta: "gerenciar os pacotes necessários e suas dependências"
+        },
+        {
+            pergunta: "Usamos props para __",
+            resposta: "passar diferentes informações para componentes"
+        },
+        {
+            pergunta: "Usamos estado (state) para __",
+            resposta: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"
+        }
     ],
     [
-        {pergunta:"Quem é o pai do Percy Jackson", resposta:"Poseidon"},
-        {pergunta:"Quem é o pai do Percy Jackson", resposta:"Poseidon"},
-        {pergunta:"Quem é o pai do Percy Jackson", resposta:"Poseidon"},
-        {pergunta:"Quem é o pai do Percy Jackson", resposta:"Poseidon"},
-        {pergunta:"Quem é o pai do Percy Jackson", resposta:"Poseidon"},
-        {pergunta:"Quem é o pai do Percy Jackson", resposta:"Poseidon"},
-        {pergunta:"Quem é o pai do Percy Jackson", resposta:"Poseidon"},
-        {pergunta:"Quem é o pai do Percy Jackson", resposta:"Poseidon"}
+        {
+            pergunta: "Quem é o pai do Percy Jackson?",
+            resposta: "Poseidon"
+        },
+        {
+            pergunta: "O que é roubado no primeiro livro?",
+            resposta: "O raio de Zeus"
+        },
+        {
+            pergunta: "Em o mar de monstros quem é o meio-irmao de Percy?",
+            resposta: "Um ciclope"
+        },
+        {
+            pergunta: "Qual objeto mágico a mãe da Anabeth deu pra ela?",
+            resposta: "Um boné de invisibilidade dos Yankees"
+        },
+        {
+            pergunta: "Como Bianca di Angelo morre?",
+            resposta: "Num dos ferros velhos de Hefesto"
+        },
+        {
+            pergunta: "Quem o velocino de ouro cura no pinheiro?",
+            resposta: "Thália, filha de Zeus. "
+        },
+        {
+            pergunta: "Quem troca Percy e Jason de acampamento ao tirar as memórias deles?",
+            resposta: "Hera"
+        },
+        {
+            pergunta: "Qual o nome da espada do Percy?",
+            resposta: "Anaklusmos"
+        }
     ]
 ];
 
